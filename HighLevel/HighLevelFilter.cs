2a2268500e61053a945ea5d3f321f0f98605a505
NikodemWspanialy <91847123+NikodemWspanialy@users.noreply.github.com>
2023-10-26 16:05:30 +0200

    To dziala nawet git

--- HighLevel/HighLevelFilter.cs
+++ HighLevel/HighLevelFilter.cs
@@ -1,35 +1,62 @@
-﻿namespace HighLevel
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace HighLevel
 {
     public class HighLevelFilter
     {
-        public static void filter(ref byte[] image, int startPoint, int endPoint)
+        private byte[] _image;
+        private long _startPoint;
+        private long _endPoint;
+
+        public HighLevelFilter(ref byte[] image, long startPoint = 0, long endPoint = 0)
         {
-            for (int i = startPoint; i < endPoint; i = i * 3)
+            _image = image;
+            _startPoint = startPoint;
+            _endPoint = endPoint;
+        }
+        public void filter()
+        {
+            
+            if (_endPoint != 0)
             {
-                int r = image[i];
-                int g = image[i + 1];
-                int b = image[i + 2];
-                double srednia = (r + b + g) / 3;
-                int sredniaint = (int)Math.Round(srednia);
-                image[i + 0] = (byte)sredniaint;
-                image[i + 1] = (byte)sredniaint;
-                image[i + 2] = (byte)sredniaint;
+
+                for (long i = _startPoint; i < _endPoint; i = i + 3)
+                {
+                    try
+                    {
+
+                    int r = _image[i];
+                    int g = _image[i + 1];
+                    int b = _image[i + 2];
+                    double srednia = (r + b + g) / 3;
+                    int sredniaint = (int)Math.Round(srednia);
+                    _image[i + 0] = (byte)sredniaint;
+                    _image[i + 1] = (byte)sredniaint;
+                    _image[i + 2] = (byte)sredniaint;
+                    }
+                    catch(Exception e)
+                    {
+                        //co tu rpboc 
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _image.Length; i += 3)
+                {
+                    int r = _image[i];
+                    int g = _image[i + 1];
+                    int b = _image[i + 2];
+                    double srednia = (r + b + g) / 3;
+                    int sredniaint = (int)Math.Round(srednia);
+                    _image[i + 0] = (byte)sredniaint;
+                    _image[i + 1] = (byte)sredniaint;
+                    _image[i + 2] = (byte)sredniaint;
+                }
+
             }
 
         }
-        public static void filter(ref byte[] image)
-        {
-            for (int i = 0; i < image.Length; i += 3)
-            {
-                int r = image[i];
-                int g = image[i + 1];
-                int b = image[i + 2];
-                double srednia = (r + b + g) / 3;
-                int sredniaint = (int)Math.Round(srednia);
-                image[i + 0] = (byte)sredniaint;
-                image[i + 1] = (byte)sredniaint;
-                image[i + 2] = (byte)sredniaint;
-            }
-        }
+    
     }
 }
